eb381b6136878a6be1881979b73407cfa8ebd186
ArkoSamad11 <arkosamad9926@gmail.com>
2026-04-19 18:22:30 -0400

    switched math to numpy, corrected theta formulas.

--- src/pricer/black_scholes.py
+++ src/pricer/black_scholes.py
@@ -1,23 +1,31 @@
 # imports
-import math as math
+import numpy as np
 from scipy.stats import norm
 
 # distance current price is from strike price
 def d1(S, K, sigma, T = (1/365), r = 0):
-    d1_value = (math.log(S / K) + (r + ((sigma)**2) / (2)) * T) / (sigma * math.sqrt(T))
+    d1_value = (np.log(S / K) + (r + ((sigma)**2) / (2)) * T) / (sigma * np.sqrt(T))
     return d1_value
+
+
 # probability adjusted distance to strike price
 def d2(S, K, sigma, T = (1/365), r = 0):
-    d2_value = d1(S, K, sigma, T, r) - (sigma * math.sqrt(T))
+    d2_value = d1(S, K, sigma, T, r) - (sigma * np.sqrt(T))
     return d2_value
+
+
 # theoretical worth of over bet 
 def call(S, K, sigma, T, r):
-    call_value = (S * norm.cdf(d1(S, K, sigma, T, r))) - (K * math.exp(-r * (T)) * norm.cdf(d2(S, K, sigma, T, r)))
+    call_value = (S * norm.cdf(d1(S, K, sigma, T, r))) - (K * np.exp(-r * (T)) * norm.cdf(d2(S, K, sigma, T, r)))
     return float(round(call_value, 3))
+
+
 # theoretical worth of under bet
 def put(S, K, sigma, T, r):
-    put_value = (K * math.exp(-r * T) * norm.cdf(-(d2(S, K, sigma, T, r)))) - (S * norm.cdf(-d1(S, K, sigma, T, r)))
+    put_value = (K * np.exp(-r * T) * norm.cdf(-(d2(S, K, sigma, T, r)))) - (S * norm.cdf(-d1(S, K, sigma, T, r)))
     return float(round(put_value, 3))
+
+
 def black_scholes(S, K, sigma, T, r):
     return (call(S, K, sigma, T, r), put(S, K, sigma, T, r))
 
--- src/pricer/greeks.py
+++ src/pricer/greeks.py
@@ -1,26 +1,42 @@
-# imports required functions for calculations of greeks
+# imports required for calculations of greeks
 from src.pricer.black_scholes import d1, d2
+from scipy.stats import norm
+import numpy as np
 
 # S represents rolling average for stat
 # theoretical change in over bet value when S moves by 1
 def call_delta(S, K, sigma, T = 1/365, r = 0):
-    pass
+    return norm.cdf(d1(S, K, sigma, T, r))
+
+
 # theoretical change in under bet value when S moves by 1
 def put_delta(S, K, sigma, T = 1/365, r = 0):
-    pass
+    return norm.cdf(d1(S, K, sigma, T, r)) - 1
+
+
 # theoretical change in delta when S moves by 1
 def gamma(S, K, sigma, T = 1/365, r = 0):
-    pass
+    return (norm.pdf(d1(S, K, sigma, T, r))) / (S * sigma * np.sqrt(T))
+
+
 # theoretical change in O/U value when volatility changes by .01
 def vega(S, K, sigma, T = 1/365, r = 0):
-    pass
+    return (S * np.sqrt(T) * norm.pdf(d1(S, K, sigma, T, r)))
+
+
 # theoretical price change per day --> slightly irrelevant because T = 1/365 for practically all bets
 def call_theta(S, K, sigma, T = 1/365, r = 0):
-    pass
+    return ((-S * norm.pdf(d1(S, K, sigma, T, r)) * sigma) / (2 * np.sqrt(T))) - (r * K * np.exp(-r * T) * norm.cdf(d2(S, K, sigma, T, r)))
+
+
 def put_theta(S, K, sigma, T = 1/365, r = 0):
-    pass
+    return ((-S * norm.pdf(d1(S, K, sigma, T, r)) * sigma) / (2 * np.sqrt(T))) + (r * K * np.exp(-r * T) * norm.cdf(-(d2(S, K, sigma, T, r))))
+
+
 # theoretical price change with risk free rate --> time to maturity is negligible, 48 minute games 
 def call_rho(S, K, sigma, T = 1/365, r=0):
-    pass
+    return (K * T * np.exp(-r * T) * norm.cdf(d2(S, K, sigma, T, r)))
+
 def put_rho(S, K, sigma, T = 1/365, r=0):
-    pass
+    return (-K * T * np.exp(-r * T) * norm.cdf(-d2(S, K, sigma, T, r)))
+

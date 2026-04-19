65835baacc2f4e2a3d76d780ca3b0ae7c6dacd4e
ArkoSamad11 <arkosamad9926@gmail.com>
2026-04-19 16:54:53 -0400

    Update black_scholes.py

---
+++ src/pricer/black_scholes.py
@@ -0,0 +1,24 @@
+# imports
+import math as math
+from scipy.stats import norm
+
+# distance current price is from strike price
+def d1(S, K, sigma, T = (1/365), r = 0):
+    d1_value = (math.log(S / K) + (r + ((sigma)**2) / (2)) * T) / (sigma * math.sqrt(T))
+    return d1_value
+# probability adjusted distance to strike price
+def d2(S, K, sigma, T = (1/365), r = 0):
+    d2_value = d1(S, K, sigma, T, r) - (sigma * math.sqrt(T))
+    return d2_value
+# theoretical worth of over bet 
+def call(S, K, sigma, T, r):
+    call_value = (S * norm.cdf(d1(S, K, sigma, T, r))) - (K * math.exp(-r * (T)) * norm.cdf(d2(S, K, sigma, T, r)))
+    return call_value
+# theoretical worth of under bet
+def put(S, K, sigma, T, r):
+    put_value = (K * math.exp(-r * T) * norm.cdf(-(d2(S, K, sigma, T, r)))) - (S * norm.cdf(-d1(S, K, sigma, T, r)))
+    return put_value
+def black_scholes(S, K, sigma, T, r):
+    return (float(call(S, K, sigma, T, r)), float(put(S, K, sigma, T, r)))
+
+print(black_scholes(27.3, 25.5, 0.18, 1/365, 0))
